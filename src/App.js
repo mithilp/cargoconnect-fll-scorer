3b1ef6629d4c2a983373bd377e020f4c51ef486c
MP <ping.mithil@gmail.com>
2021-07-17 15:37:08 -0700

    Create Light/Dark Mode Button (Hidden until Flashing Issue Fix)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-	return <ChakraProvider>added in chakra ui</ChakraProvider>;
+	return (
+		<ChakraProvider>
+			added in chakra ui
+			{/* TODO: add in <ColorModeSwitcher /> when Chakra UI fixes flashing issue
+				Learn more:
+					https://chakra-ui.com/docs/features/color-mode#color-mode-flash-issue */}
+		</ChakraProvider>
+	);
 }
 
 export default App;
---
+++ src/components/ColorModeSwitcher.js
@@ -0,0 +1,32 @@
+import {
+  useColorMode,
+  useColorModeValue,
+  Tooltip,
+  IconButton,
+} from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+export const ColorModeSwitcher = props => {
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <Tooltip
+      label={`Switch to ${text} mode`}
+      aria-label={`Switch to ${text} mode`}
+    >
+      <IconButton
+        size="md"
+        borderRadius="md"
+        fontSize="lg"
+        aria-label={`Switch to ${text} mode`}
+        variant="ghost"
+        color="current"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+        {...props}
+      />
+    </Tooltip>
+  );
+};
